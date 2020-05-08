23263842e70f2ef6fe9acf7165fa327b7350e29e
Elephant <sby-kov@mail.ru>
2020-05-08 17:01:32 +0700

    Commit 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * User:  Marina
+ * Дата:  08.05.2020
+ * Время: 16:36
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello? World!");
+    }
+}
