b44b8ed022e47e979136da1093d43c470adae823
sby-kov <32094353+sby-kov@users.noreply.github.com>
2020-05-08 17:28:16 +0700

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,8 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("т.е. Привет, Мир!");
+        System.out.print("Hello, World!");
+        System.out.println("  т.е. Привет, Мир!");
+        System.out.println(" Ура! Работает pull...");
     }
 }
